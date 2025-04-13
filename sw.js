a641881e7ce2978469f378e5833c36e2d879be70
Kotakovec <kotakjakub@gmail.com>
2025-04-13 08:43:02 +0200

    1

--- sw.js
+++ sw.js
@@ -2,21 +2,21 @@ const CACHE_NAME = 'pokracovat-app-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/styles.css',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.url.includes('nicepage.io')) {
-    event.respondWith(fetch(event.request));
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => response || fetch(event.request))
-  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
